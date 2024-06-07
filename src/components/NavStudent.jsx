86faa729727768ee0fc21c3179603a44bbabc7c5
AnnaJ <annaspaulimp@gmail.com>
2024-06-07 15:00:33 +0530

    updated

--- src/components/NavStudent.jsx
+++ src/components/NavStudent.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavStudent = () => {
     return (
         <div>
 
-            <nav class="navbar navbar-expand-lg bg-warning">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg bg-warning">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/search">Search</a>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/search">Search</Link>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/delete">Delete</a>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/delete">Delete</Link>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/viewall">View All Students</a>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/viewall">View All Students</Link>
                             </li>
                         </ul>
                     </div>
