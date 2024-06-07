407458a8354634e21e8c90ea20796c9ae4a42ef3
AnnaJ <annaspaulimp@gmail.com>
2024-06-07 14:32:46 +0530

    Successful

---
+++ src/components/ViewAllStudents.jsx
@@ -0,0 +1,123 @@
+import React from 'react'
+import NavStudent from './NavStudent'
+
+const ViewAllStudents = () => {
+    return (
+        <div>
+            <NavStudent/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col-12 col-sm-6 col-md-4 col-lg-4 col-xl 3 col-xxl-3">
+
+                                <div class="card">
+                                    <img height="300px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_85t0H4sD9dihzdpaaKtezIpBhE16Mjhipg&s" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Apsara</h5>
+                                        <p class="card-text">Student of class 10</p>
+                                        <a href="#" class="btn btn-primary">Check details</a>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-4 col-lg-4 col-xl 3 col-xxl-3">
+                                <div class="card">
+                                    <img height="300px" src="https://www.shutterstock.com/shutterstock/photos/2071252046/display_1500/stock-photo-portrait-of-cheerful-male-international-indian-student-with-backpack-learning-accessories-standing-2071252046.jpg" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Anil</h5>
+                                        <p class="card-text">Student of BCA</p>
+                                        <a href="#" class="btn btn-primary">Check details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-4 col-lg-4 col-xl 3 col-xxl-3">
+                                <div class="card">
+                                    <img height="300px" src="https://img.freepik.com/free-photo/woman-looking-camera-while-holding-some-books_23-2148311817.jpg" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Aina</h5>
+                                        <p class="card-text">Student of BCA</p>
+                                        <a href="#" class="btn btn-primary">Check details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-4 col-lg-4 col-xl 3 col-xxl-3">
+                                <div class="card">
+                                    <img height="300px" src="https://www.shutterstock.com/image-photo/confident-handsome-student-holding-books-260nw-366568778.jpg" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Morwan</h5>
+                                        <p class="card-text">Student of 12</p>
+                                        <a href="#" class="btn btn-primary">Check details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-4 col-lg-4 col-xl 3 col-xxl-3">
+                                <div class="card">
+                                    <img height="300px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS28xaaTZoJndiKATqrwl4_4S5v5fGa-8IH0nwTquV3VKHm0CcqniaialLytOFm0LANCHs&usqp=CAU" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Gabia</h5>
+                                        <p class="card-text">Student of class 10</p>
+                                        <a href="#" class="btn btn-primary">Check details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-4 col-lg-4 col-xl 3 col-xxl-3">
+                                <div class="card">
+                                    <img height="300px" src="https://c8.alamy.com/comp/ED1GCN/1-indian-child-school-student-showing-ED1GCN.jpg" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Bibin</h5>
+                                        <p class="card-text">Student of class 5</p>
+                                        <a href="#" class="btn btn-primary">Check details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-4 col-lg-4 col-xl 3 col-xxl-3">
+                                <div class="card">
+                                    <img height="300px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUqwFHz01SJrWu_FkYmG85pak2SRINFQZRAE07_zETWQt4WJPC3cySWChVB6M3mlvgIRI&usqp=CAU" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Anamika</h5>
+                                        <p class="card-text">Student of class MCom</p>
+                                        <a href="#" class="btn btn-primary">Check details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-4 col-lg-4 col-xl 3 col-xxl-3">
+                                <div class="card">
+                                    <img height="300px" src="https://img.freepik.com/free-photo/surprised-young-student-boy-wearing-back-bag-glasses-cap-holding-books-putting-hand-chest-white_141793-74750.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1705363200&semt=ais" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Esahak</h5>
+                                        <p class="card-text">Student of class MCom</p>
+                                        <a href="#" class="btn btn-primary">Check details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-4 col-lg-4 col-xl 3 col-xxl-3">
+                                <div class="card">
+                                    <img height="300px" src="https://c8.alamy.com/comp/ED1GC9/1-indian-child-school-student-ED1GC9.jpg" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Shibu</h5>
+                                        <p class="card-text">Student of class 7</p>
+                                        <a href="#" class="btn btn-primary">Check details</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-4 col-lg-4 col-xl 3 col-xxl-3">
+                                <div class="card">
+                                    <img height="300px" src="https://c8.alamy.com/comp/EFD284/1-indian-girl-school-student-victory-trophy-EFD284.jpg" class="card-img-top" alt="..."></img>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Lisa</h5>
+                                        <p class="card-text">Student of class 10</p>
+                                        <a href="#" class="btn btn-primary">Check details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ViewAllStudents
